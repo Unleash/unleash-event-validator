0afc4127c4df1583ef5d7fe67bf68f475e9c8fc4
Christopher Kolstad <chriswk@getunleash.ai>
2021-11-17 13:22:47 +0100

    Converted to typescript

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const Ajv2020 = require("ajv/dist/2020");
-const addFormats = require('ajv-formats');
-const ajv = new Ajv2020({allErrors: true});
-const unleashSchema = require('./schemas/unleash-schema.json');
-addFormats(ajv);
-const validator = ajv.compile(unleashSchema);
-
-module.exports = {
-    validator
-};
+"use strict";
+exports.__esModule = true;
+var _2020_1 = require("ajv/dist/2020");
+var ajv_formats_1 = require("ajv-formats");
+var ajv = new _2020_1["default"]({ allErrors: true });
+var unleashSchema = require("./schemas/unleash-schema.json");
+ajv_formats_1["default"].addFormats(ajv);
+var validator = ajv.compile(unleashSchema);
+exports["default"] = validator;
